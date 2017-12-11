25e1dc4d0983779780e03ef549dcdd8396336961
Mark Fitzpatrick <markfitz815@gmail.com>
2017-12-11 17:57:13 +0000

    Further learning about components

--- work/main.js
+++ work/main.js
@@ -1,3 +1,17 @@
+Vue.component('task-list', {
+    template: '<div><task v-for="task in tasks" :key="task.id">{{ task.task}}</task></div>',
+
+    data(){
+        return{
+            tasks: [
+                {task: 'Go to the moon', complete: true, id: 1},
+                {task: 'Let the dog out', complete: false, id: 2},
+                {task: 'Call Irish Water', complete: false, id: 3}
+            ]
+        }
+    }
+});
+
 Vue.component('task', {
 
     template: '<li><slot></slot></li>',
