2978b18b7de86d686775511a82f48be356c0a9e6
Mark Fitzpatrick <markfitz815@gmail.com>
2018-03-30 15:28:11 +0100

    completed project and added functionality

--- vuejs-playlist/punchBagGame/punchBagScript.js
+++ vuejs-playlist/punchBagGame/punchBagScript.js
@@ -3,7 +3,21 @@ console.log('app')
 
 new Vue({
     el: '#vue-app',
-    data: {},
-    methods: {},
+    data: {
+        health: 100,
+        ended: false
+    },
+    methods: {
+        punch: function(){
+            this.health -= 10;
+            if(this.health <= 0){
+                this.ended = true;
+            }
+        },
+        restart: function(){
+            this.health = 100;
+            this.ended = false;
+        }
+    },
     computed: {}
 })
